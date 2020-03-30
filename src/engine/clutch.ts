839ad55616d258903a6c9c439fde3eda5ccd690f
tiltproofRain <srijannr1@gmail.com>
2020-03-30 15:57:49 +0530

    added a 2D vector library

--- src/engine/clutch.ts
+++ src/engine/clutch.ts
@@ -1,7 +1,9 @@
 import Game from './game/game.js'
+import Vector2 from './vector./vector.js';
 
 const Clutch = {
-    Game: Game
+    Game: Game,
+    Vector2: Vector2
 }
 
 export default Clutch;
--- src/engine/game/game.ts
+++ src/engine/game/game.ts
@@ -5,10 +5,10 @@ export default class Game {
   ctx;
   constructor(config = { width: 480, height: 320 }, parent = document.body) {
     this.config = config;
-    this.canvas = document.createElement("canvas");
+    this.canvas = document.createElement('canvas');
     this.canvas.setAttribute('width', config.width);
     this.canvas.setAttribute('height', config.height);
     parent.appendChild(this.canvas);
-    this.ctx = this.canvas.getContext("2d");
+    this.ctx = this.canvas.getContext('2d');
   }
 }
